fd1f7b51b1df016532d6d2fdf9fbd5badb9dcba0
Alan Chou <danielzhou8291@gmail.com>
2023-06-06 14:25:07 +0800

    Update for video

--- main.js
+++ main.js
@@ -4,6 +4,53 @@ let close4=document.querySelector("#close4");
 close4.addEventListener("click", function(){
   video.close();
 })
+let player; //YouTube Player
+let currentPlay = 0; //記錄目前撥到第幾首歌
+//YouTube API Ready
+function onYouTubeIframeAPIReady(){
+    player = new YT.Player("player",{
+        height:"390",
+        width:"640",
+        videoId:playList[currentPlay],
+        playerVars:{
+        autoplay:0, //是否自動撥放
+        controls:1, //是否顯示控制項
+        start:playTime[currentPlay][0],//開始秒數
+        end:playTime[currentPlay][1],//結束秒數
+        iv_load_policy:3
+    },
+    events:{
+        onReady:onPlayerReady,
+        onStateChange:onPlayerStateChange
+    }
+        });
+        
+}
+//YouTube Player Ready
+function onPlayerReady(event){
+}
+//Player State Change
+function onPlayerStateChange(event){
+    if(Math.floor(player.getCurrentTime())==playTime[currentPlay][1]){
+        if(currentPlay<playList.length-1){
+            currentPlay++;
+            player.loadVideoById({
+                videoId:playList[currentPlay],
+                startSeconds:playTime[currentPlay][0],
+                endSeconds:playTime[currentPlay][1],
+                suggestedQuality:"large"
+            });
+        }else{
+            currentPlay = 0;
+            player.cueVideoById({
+                videoId:playList[currentPlay],
+                startSeconds:playTime[currentPlay][0],
+                endSeconds:playTime[currentPlay][1],
+                suggestedQuality:"large"
+            });
+        }
+    }
+}
 //mapArray - 決定地圖中每個格子的元素
 //ctx - HTML5 Canvas用
 //currentImgMainX, currentImgMainY - 決定主角所在座標
