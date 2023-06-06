e526f0c51977081d210d82d7f96716df27ff85ab
Alan Chou <danielzhou8291@gmail.com>
2023-06-06 14:11:11 +0800

    Update for video

--- yt.js
+++ yt.js
@@ -45,15 +45,3 @@ function onPlayerStateChange(event){
         }
     }
 }
-
-let playList = [
-    "q6EoRBvdVPQ",
-    "QH2-TGUlwu4",
-    "DNR2AY7b6kI"
-];
-//播放起訖秒數
-let playTime = [
-    [0,9],
-    [65,70],
-    [15,21]
-];
